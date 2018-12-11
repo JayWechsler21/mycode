6f29b9374c2907359324fc4e3a0e7300e84a1d6b
Jay Wechsler <jay.wechsler@verizonwireless.com>
2018-12-11 09:34:25 -0500

    lab7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+#Written By: Jay "The Wex" Wechsler
+#Collect IP:address information from a user, and display it back to them.
+
+##Colect input from the user
+user_input = input('Please enter an IPv4 IP address:')
+
+##Print out the input collected from the user
+print("You told me the IPv4 address is:" + user_input)
+
+
+##The first line should collect and store input from the user. Ask the user for the ‘vendor name’ associated with the device. Use any variable name you would like.
+##Use a second line of code to print the input you just collected from the user.
+
+##Ask the user for the vendor name
+vendor_name= input('vendor name')
