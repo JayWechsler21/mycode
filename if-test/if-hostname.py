a759d361c59321257f88ddc045370023a75d79a2
Jay Wechsler <jay.wechsler@verizonwireless.com>
2018-12-12 09:13:41 -0500

    lab13

--- if-test/if-hostname.py
+++ if-test/if-hostname.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-hostname = 'MTG'
+
+################################
+#Asking what the host name is
+################################
+prompt_host_name_msg = "What is the hostname?"  
+#################################
+#typing in hostname
+#################################
+hostname = (input (prompt_host_name_msg))
+hostname= hostname.upper()
 if hostname == 'MTG':
     print('The hostname was found to be mtg')
+    print('hostname matches expected config')
+    print('Exiting the script.')
